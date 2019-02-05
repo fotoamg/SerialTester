a7b3207a55043983ef4c807eec479d7f436b8552
Betti <betti0630@hotmail.com>
2019-02-05 22:43:17 +0100

    initial commit

---
+++ SerialWriter/FormData.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerialWriter
+{
+    public class FormData : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string FileNotPath
+        {
+            get { return (String.IsNullOrEmpty(this.FileName) ? "" : this.FileName.Substring(this.FileName.LastIndexOf("\\")+1)); }
+           
+        }
+        /*  private string replyText;
+
+          public string ReplyText
+          {
+              get { return this.replyText; }
+              set
+              {
+                  this.replyText = value;
+                  OnPropertyChanged("ReplyText");
+              }
+          }
+          public void AppendReplyText(String text)
+          {
+                  this.replyText += text;
+                  OnPropertyChanged("ReplyText");
+
+          }*/
+
+        private PortData[] portList;
+
+        public PortData[] PortList
+        {
+            get { return this.portList; }
+            set
+            {
+                this.portList = value;
+                OnPropertyChanged("PortList");
+            }
+        }
+
+        private PortData port;
+
+        public PortData Port
+        {
+            get { return this.port; }
+            set
+            {
+                this.port = value;
+                OnPropertyChanged("Port");
+            }
+        }
+
+        private int delay;
+
+        public int Delay
+        {
+            get { return this.delay; }
+            set
+            {
+                this.delay = value;
+                OnPropertyChanged("Delay");
+            }
+        }
+
+        private string fileName;
+        public string FileName
+        {
+            get { return this.fileName; }
+            set
+            {
+                this.fileName = value;
+                OnPropertyChanged("FileName");
+            }
+        }
+
+        public bool Stop { get; set; }
+
+      
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+
+
+        }
+    }
+
+    public class PortData
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ SerialWriter/MainWindow.xaml.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+
+namespace SerialWriter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private SerialPort port;
+        private FormData formData; 
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            formData = new FormData();
+            this.DataContext = formData;
+            var portList = SerialPort.GetPortNames();
+            formData.PortList = portList.Select(x => new PortData {Name = x}).ToArray();
+            formData.Delay = 400;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+ 
+            var message = String.Format("{0}\n", messageTextBox.Text);
+            SendMessageToPort(message);
+            this.Dispatcher.Invoke(() =>
+            {
+                messageTextBox.Clear();
+            });
+
+
+        }
+
+        private void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            ReadPort(port);
+        }
+
+        private void SendMessageToPort(string message)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(message);
+            if (port != null && port.IsOpen)
+            {
+                port.Write(data, 0, data.Length);
+                this.Dispatcher.BeginInvoke(
+                System.Windows.Threading.DispatcherPriority.Normal,
+                (UpdateTheUI)delegate (string text)
+                {
+                    fileMessageTextBox.Text += text;
+                    fileMessageTextBox.ScrollToEnd();
+                }, message);
+            }
+            else
+            {
+                MessageBox.Show("Port is not opened.");
+            }
+
+        }
+
+        private void ReadPort(SerialPort readPort)
+        {
+            if (readPort.BytesToRead > 0)
+            {
+                var bufferSize = readPort.BytesToRead;
+                if (bufferSize > 80)
+                {
+                    bufferSize = 80;
+                }
+                //Buffer with data
+                byte[] readData = new byte[bufferSize];
+
+                //Handle data
+                readPort.Read(readData, 0, readData.Length);
+                var reply = Encoding.ASCII.GetString(readData);
+
+                this.Dispatcher.BeginInvoke(
+                       System.Windows.Threading.DispatcherPriority.Normal,
+                       (UpdateTheUI)delegate (string text)
+                       {
+                           replyTextBox.Text += text;
+                            replyTextBox.ScrollToEnd();
+                       }, reply);
+
+               /* formData.AppendReplyText(reply);
+                this.Dispatcher.Invoke(() =>
+                {
+                    //replyTextBox.Text += reply + "\n" + DateTime.Now. + "\n";
+                    replyTextBox.ScrollToEnd();
+                });*/
+            }
+            if (readPort.BytesToRead > 0) ReadPort(readPort);
+
+        }
+
+        private void openFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            //formData.Stop = false;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                formData.FileName = openFileDialog.FileName;
+                this.Dispatcher.Invoke(() =>
+                {
+                    fileNameTextBox.Text = formData.FileNotPath;
+                   fileNameTextBox.ScrollToEnd();
+                }); 
+                //Thread thrd = new Thread(() => WriteReply(openFileDialog.FileName));
+                //thrd.Start();
+            }
+
+        }
+
+        private void WriteReply(string fileName)
+        {
+            int delay = formData.Delay;
+            var line = "";
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(fileName);
+            var lineCounter = 0;
+            var maxLine = 2;
+            var message = String.Empty;
+            while ((line = file.ReadLine()) != null && !formData.Stop)
+            {
+                message += String.Format("{0}\n", line);
+                if (lineCounter < maxLine - 1)
+                {
+                    lineCounter++;
+                }
+                else
+                {
+                    lineCounter = 0;
+                    Thread.Sleep(delay);
+                    SendMessageToPort(message);
+
+                    //this.Dispatcher.BeginInvoke(
+                    //    System.Windows.Threading.DispatcherPriority.Normal,
+                    //    (UpdateTheUI) delegate(string text)
+                    //    {
+                    //        fileMessageTextBox.Text += text;
+                    //        fileMessageTextBox.ScrollToEnd();
+                    //    }, message);
+
+                    message = "";
+                }
+            }
+            if (!String.IsNullOrEmpty(message))
+            {
+                Thread.Sleep(delay);
+                SendMessageToPort(message);
+            }
+
+            file.Close();
+
+
+        }
+
+        delegate void UpdateTheUI(string newText);
+
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            fileMessageTextBox.Clear();
+            replyTextBox.Clear();
+        }
+
+        private void portComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //var defaultPortName = "COM5";
+            if (port != null && port.IsOpen)
+            {
+                port.Close();
+            }
+
+                port = new SerialPort(portComboBox.SelectedValue.ToString());
+                port.BaudRate = 9600;
+                port.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+                port.Open();
+
+        }
+
+        private void stopButton_Click(object sender, RoutedEventArgs e)
+        {
+            formData.Stop = true;
+        }
+
+        private void replyButton_Click(object sender, RoutedEventArgs e)
+        {
+            formData.Stop = false;
+            Thread thrd = new Thread(() => WriteReply(formData.FileName));
+            thrd.Start();
+        }
+    }
+
+
+}
